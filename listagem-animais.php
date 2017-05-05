19b9e54f03ab033d4521964bb5fe5b1b86e11860
root <deividy.n@gmail.com>
2017-05-04 22:05:32 -0300

    arquivo de listagem de animais

---
+++ listagem-animais.php
@@ -0,0 +1,221 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="UTF-8">
+	<title>Listagem de animais</title>
+	<link rel="stylesheet" href="css/foundation.css">
+	<link rel="stylesheet" href="lightbox2-master/src/css/lightbox.css">
+</head>
+<body>
+
+	<div class="row">
+		<div class="small-12 columns">
+			<h1>
+				Listagem de Animais
+			</h1>
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="small-12 columns">
+			<form action="#" method="#">
+				<div class="row collapse">
+					<div class="small-12 medium-8 large-8 columns">
+						<input type="text" placeholder="Pesquisar" name="pesquisar">
+					</div>
+					<div class="small-12 medium-4 large-4 columns">
+						<input type="submit" value="Pesquisar" class="button success">
+					</div>
+				</div>
+				
+			</form>
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="small-12 columns">
+			<h2>Resultados da busca para: "Gato"</h2>
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="small-12 medium-3 large-3 columns">
+			<a href="pics/gato.png" data-lightbox="image-gato"> 
+				<img src="pics/gato.png" alt="Gato">
+			</a>
+		</div>
+		<div class="small-12 medium-4 large 4 columns">
+			<div class="row">
+				<div class="small-12 columns">
+					<a href="#">
+						Nome: Polenta
+					</a>
+				</div>
+			</div>
+			<div class="row">
+				<div class="small-12 columns">
+					<a href="#">
+						Tipo: Gato
+					</a>
+				</div>
+			</div>
+			<div class="row">
+				<div class="small-12 columns">
+					<a href="#">
+						Raça: Vira lata
+					</a>
+				</div>
+			</div>
+		</div>
+		<div class="small-12 medium-5 large-5 columns">
+			<div class="row">
+				<div class="small-12 columns">
+					<a href="#">
+						Porte: Pequeno
+					</a>
+				</div>
+			</div>
+			<div class="row">
+				<div class="small-12 columns">
+					<a href="#">
+						Idade: 19
+					</a>
+				</div>
+			</div>
+			<div class="row">
+				<div class="small-12 columns">
+					<a href="#">
+						Proprietário: Restaurante xing ling
+					</a>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="small-12 medium-3 large-3 columns">
+			<a href="pics/gato.png" data-lightbox="image-gato2"> 
+				<img src="pics/gato.png" alt="Gato">
+			</a>
+		</div>
+		<div class="small-12 medium-4 large 4 columns">
+			<div class="row">
+				<div class="small-12 columns">
+					<a href="#">
+						Nome: Polenta
+					</a>
+				</div>
+			</div>
+			<div class="row">
+				<div class="small-12 columns">
+					<a href="#">
+						Tipo: Gato
+					</a>
+				</div>
+			</div>
+			<div class="row">
+				<div class="small-12 columns">
+					<a href="#">
+						Raça: Vira lata
+					</a>
+				</div>
+			</div>
+		</div>
+		<div class="small-12 medium-5 large-5 columns">
+			<div class="row">
+				<div class="small-12 columns">
+					<a href="#">
+						Porte: Pequeno
+					</a>
+				</div>
+			</div>
+			<div class="row">
+				<div class="small-12 columns">
+					<a href="#">
+						Idade: 19
+					</a>
+				</div>
+			</div>
+			<div class="row">
+				<div class="small-12 columns">
+					<a href="#">
+						Proprietário: Restaurante xing ling
+					</a>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="small-12 medium-3 large-3 columns">
+			<a href="pics/gato.png" data-lightbox="image-gato3"> 
+				<img src="pics/gato.png" alt="Gato">
+			</a>
+		</div>
+		<div class="small-12 medium-4 large 4 columns">
+			<div class="row">
+				<div class="small-12 columns">
+					<a href="#">
+						Nome: Polenta
+					</a>
+				</div>
+			</div>
+			<div class="row">
+				<div class="small-12 columns">
+					<a href="#">
+						Tipo: Gato
+					</a>
+				</div>
+			</div>
+			<div class="row">
+				<div class="small-12 columns">
+					<a href="#">
+						Raça: Vira lata
+					</a>
+				</div>
+			</div>
+		</div>
+		<div class="small-12 medium-5 large-5 columns">
+			<div class="row">
+				<div class="small-12 columns">
+					<a href="#">
+						Porte: Pequeno
+					</a>
+				</div>
+			</div>
+			<div class="row">
+				<div class="small-12 columns">
+					<a href="#">
+						Idade: 19
+					</a>
+				</div>
+			</div>
+			<div class="row">
+				<div class="small-12 columns">
+					<a href="#">
+						Proprietário: Restaurante xing ling
+					</a>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+
+	
+
+
+	<div class="row">
+		<div class="small-12 medium-4 large-4 medium-centered columns">
+			<span>Anterior</span>
+			<span>1...2</span>
+			<span>Próximo</span>
+		</div>
+	</div>
+
+
+	<script src="js/vendor/jquery.js"></script>
+	<script src="js/vendor/foundation.js"></script>
+	<script src="lightbox2-master/src/js/lightbox.js"></script>
+</body>
+</html>
