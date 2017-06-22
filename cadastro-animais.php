3791918141e5289cde1928beb2ceebf3d4a8776a
acacia <terra.acacia@gmail.com>
2017-06-22 20:51:24 -0300

    Aplicação de padrão de layout no cadastro de animais

--- cadastro-animais.php
+++ cadastro-animais.php
@@ -41,36 +41,44 @@ mysqli_close($link);
 </head>
 <body>
 
-  <div class="row">
-		<div class="small-12 columns small-centered">
-			<h1>Cadastro De Animais</h1>
-		</div>
-	</div>
 
-  <div class="row">
-  	<div class="small-12 medium-12 large-12 columns">
-  		<form action="cadastro-animais.php" method="post">
+  <div style="margin-top:1%" class="row small-12 medium-5 large-5 column">
+
+    <div class="row">
+      <div class="small-12 medium-12 large-12 columns small-centered text-center">
+        <h3 style="color:#767676">Cadastro De Animais</h3>
+      </div>
+    </div>
+
+  <!-- <div class="row">
+  	<div class="small-12 medium-12 large-12 columns"> -->
+  		<form  style="margin-top:3%" action="cadastro-animais.php" method="post">
 		    <div class="row">
-          <div class="small-12 medium-4 large-4 columns">
+          <div class="small-12 medium-12 large-12 columns">
     		    <label>Nome do animal
     		    <input placeholder="Digite apenas letras" required pattern="[a-z\s]+$" type="text" name="nomeanimal">
             </label>
           </div>
-		    	<fieldset class="small-12 medium-4 large-4 columns">
+        </div>
+
+        <div class="row">
+		    	<fieldset class="small-12 medium-8 large-8 columns">
               <legend>Escolha o porte/tamanho do animal</legend>
 				    <input type="radio" name="porte" value="pequeno" checked> <label for="pequeno">Pequeno</label>
 				    <input type="radio" name="porte" value="medio"> <label for="medio">Médio</label>
 				    <input type="radio" name="porte" value="grande"> <label for="grande">Grande</label>
           </fieldset>
           <div class="small-12 medium-4 large-4 columns">
-            <label>Imagem do animal
-            <input type="file" name="foto">
-            </label>
+                <fieldset>
+                      <legend>Amigavel ?</legend>
+                      <input type="radio" name="amigavel" value="pequeno" checked> <label for="TRUE">Sim</label>
+                      <input type="radio" name="amigavel" value="medio"> <label for="NULL">Não</label>
+                </fieldset>
           </div>
         </div>
 
-        <div class="row">
-          <div class="small-12 medium-3 large-3 columns">
+      <div class="row">
+          <div class="small-12 medium-4 large-4 columns">
 		         <label>Tipo de animal<!--Buscar no banco-->
 		          <select required name="tipoanimal">
                       <option value="">Selecione</option>
@@ -86,48 +94,48 @@ mysqli_close($link);
 		          </select>
             </label>
           </div>
-          <div class="small-12 medium-3 large-3 columns">
+          <div class="small-12 medium-4 large-4 columns">
             <label>Raça <!--Buscar no banco as raças de acordo com o tipo de animal-->
             <select required name="raca"></select>
             </label>
           </div>
 
-          <div class="small-12 medium-3 large-3 columns">
+          <div class="small-12 medium-4 large-4 columns">
 		    		<label>Idade:
 		   			 <input required type="number" name="idadeanimal" min="0" max="200">
             </label>
 		    	</div>
-
-                  <div class="small-12 medium-3 large-3 columns">
-                        <fieldset>
-                              <legend>Amigavel ?</legend>
-                              <input type="radio" name="amigavel" value="pequeno" checked> <label for="TRUE">Sim</label>
-                              <input type="radio" name="amigavel" value="medio"> <label for="NULL">Não</label>
-                        </fieldset>
-                  </div>
         </div>
   		    <!--No momento de inserir no banco, o campo "usuario" deve ser pego da sessão-->
         <div class="row">
         <div class="small-12 medium-12 large-12 columns">
   		    <label>Descrição
-  		    <textarea name="descricao" placeholder="Informe mais detalhes sobre o animal (idade, histórico de saúde, preferência de que tipo de lar, etc...)"></textarea>
+  		    <textarea name="descricao" rows="10" placeholder="Informe mais detalhes sobre o animal (idade, histórico de saúde, preferência de que tipo de lar, etc...)"></textarea>
+          </label>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="small-12 medium-6 large-6 columns">
+          <label>Imagem do animal
+            <input type="file" name="foto">
           </label>
         </div>
       </div>
 
       <div class="row">
       <div class="small-12 medium-12 large-12 columns">
-            <input name="Vraiva" id="Vraiva" type="checkbox"><label for="checkbox2">Tem vacina para raiva ?</label>
+            <input name="Vraiva" id="Vraiva" type="checkbox"><label for="checkbox2">Este animal já tomou a vacina para <strong>raiva</strong></label>
       </div>
       </div>
       <div class="row">
       <div class="small-12 medium-12 large-12 columns">
-            <input name="v8v10" id="v8v10" type="checkbox"><label for="checkbox2">Tem vacina v8v10 ?</label>
+            <input name="v8v10" id="v8v10" type="checkbox"><label for="checkbox2">Este animal já tomou a vacina <strong>v8v10</strong></label>
       </div>
       </div>
       <div class="row">
       <div class="small-12 medium-12 large-12 columns">
-      <input name="castrado" id="castrado" type="checkbox"><label for="checkbox2">Castrado ?</label>
+      <input name="castrado" id="castrado" type="checkbox"><label for="checkbox2">Este animal já está <strong>castrado</strong></label>
       </div>
       </div>
 
@@ -138,8 +146,8 @@ mysqli_close($link);
         </div>
       </div>
 		 </form>
-  	</div>
-  </div>
+  	<!-- </div>
+  </div> -->
 
 
     <script src="js/vendor/jquery.js"></script>
